ff06d3580c4c65909c8c9d646866abaeefa0fdbc
Amanda <amandalkmim@gmail.com>
2024-09-17 18:02:44 -0300

    New way of adding images to view

--- gallery/views.py
+++ gallery/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from gallery.models import Photography
 
 def index(request):    
-    photography = Photography.objects.all()
+    photography = Photography.objects.order_by('datetime').filter(published=True)
 
     return render(request, 'gallery/index.html', {'cards' : photography})
 
 def image(request, photography_id):
     photography = get_object_or_404(Photography, pk=photography_id)
     return render(request, 'gallery/image.html', {'photography': photography})
+
+def search(request):
+    return render(request, 'gallery/search.html')
