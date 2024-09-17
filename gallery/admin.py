41224a592de2bb81e8cd8bd734497e5992e29ad4
Amanda <amandalkmim@gmail.com>
2024-09-17 18:01:09 -0300

    Configure Admin and make changes in database

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from gallery.models import Photography
 
-# Register your models here.
+class ListingPhotographys(admin.ModelAdmin):
+    list_display = ('id', 'name', 'caption', 'published')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_filter = ('category',)
+    list_per_page = 10
+    list_editable = ('published',)
+
+admin.site.register(Photography, ListingPhotographys)
--- gallery/models.py
+++ gallery/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from datetime import datetime
 
 class Photography(models.Model):
+    OPCOES_CATEGORY = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALÁXIA', 'Galáxia'),
+        ("PLANETA", 'Planeta')
+    ]
+
     name = models.CharField(max_length=100, null=False, blank=False)
     caption = models.CharField(max_length=150, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
-    photo = models.CharField(max_length=100, null=False, blank=False)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    category = models.CharField(max_length=100, choices=OPCOES_CATEGORY, default='')
+    published = models.BooleanField(default=False)
+    datetime = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self) -> str:
-        return f'Photography [name={self.name}]'
+        return self.name
