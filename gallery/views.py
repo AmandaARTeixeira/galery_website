582215f88ffc44835b48e9025c1e8bfb3678c161
Amanda <amandalkmim@gmail.com>
2024-09-16 23:37:15 -0300

    Create a database with the photos information and shows each photo in their respective page

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from gallery.models import Photography
 
-def index(request):
-    return render(request, 'gallery/index.html')
+def index(request):    
+    photography = Photography.objects.all()
 
-def image(request):
-    return render(request, 'gallery/image.html')
+    return render(request, 'gallery/index.html', {'cards' : photography})
+
+def image(request, photography_id):
+    photography = get_object_or_404(Photography, pk=photography_id)
+    return render(request, 'gallery/image.html', {'photography': photography})
