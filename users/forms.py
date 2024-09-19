25e72dc01e5f8df6a0cce5fe98ee11dfeef9ef8d
Amanda <amandalkmim@gmail.com>
2024-09-19 20:16:17 -0300

    Create Login and Register views using forms

---
+++ users/forms.py
@@ -0,0 +1,74 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    name_login=forms.CharField(
+        label='Username', 
+        required=True, 
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+               'class': 'form-control',
+               'placeholder' : 'Ex: John Wallace'
+            }
+        )
+        )
+    password = forms.CharField(
+        label='Password', 
+        required=True, 
+        max_length=100, 
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder' : 'Type your password'
+            }
+        )
+        )
+    
+class RegisterForm(forms.Form):
+    name_login=forms.CharField(
+        label='Username', 
+        required=True, 
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+               'class': 'form-control',
+               'placeholder' : 'Ex: John Wallace'
+            }
+        )
+        )
+
+    email = forms.EmailField(
+        label='Email', 
+        required=True, 
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+               'class': 'form-control',
+               'placeholder' : 'johnwallace@example.com'
+            }
+        )
+        ) 
+
+    password1 = forms.CharField(
+        label='Password', 
+        required=True, 
+        max_length=100, 
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder' : 'Type your password'
+            }
+        )
+        )  
+    
+    password2 = forms.CharField(
+        label='Confirm your password', 
+        required=True, 
+        max_length=100, 
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder' : 'Type your password again'
+            }
+        )
+        )  
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from users.forms import LoginForm, RegisterForm
 
 def login(request):
-    return render(request, 'users/login.html')
+    form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
 
 def register(request):
-    return render(request, 'users/register.html')
+    form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
 def logout(request):
     return render(request, 'users/logout.html')
