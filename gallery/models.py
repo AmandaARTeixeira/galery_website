e2472651d82e0af4760099a5b9eb43ae7d3c241f
Amanda <amandalkmim@gmail.com>
2024-09-22 01:06:59 -0300

    Make it possible to add who posted the photo

--- gallery/models.py
+++ gallery/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 class Photography(models.Model):
     OPCOES_CATEGORY = [
-        ('NEBULOSA', 'Nebulosa'),
-        ('ESTRELA', 'Estrela'),
-        ('GALÁXIA', 'Galáxia'),
-        ("PLANETA", 'Planeta')
+        ('MIXED', 'Mixed'),
+        ('MAINECOON', 'Maine Coon')
     ]
 
     name = models.CharField(max_length=100, null=False, blank=False)
@@ -16,6 +15,7 @@ class Photography(models.Model):
     category = models.CharField(max_length=100, choices=OPCOES_CATEGORY, default='')
     published = models.BooleanField(default=False)
     datetime = models.DateTimeField(default=datetime.now, blank=False)
+    posted_by = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, blank=False, related_name='user')
 
     def __str__(self) -> str:
         return self.name
